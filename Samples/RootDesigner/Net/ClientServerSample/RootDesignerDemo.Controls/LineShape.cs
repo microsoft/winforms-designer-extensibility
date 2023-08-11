637ee0355f70579ec08a48b91c25dd84a10bb298
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2023-08-11 12:55:21 -0700

    Introduce basic Shape classes.

---
+++ Samples/RootDesigner/Net/ClientServerSample/RootDesignerDemo.Controls/LineShape.cs
@@ -0,0 +1,7 @@
+﻿namespace RootDesignerDemo.Designer.Server;
+
+public class LineShape : ShapeBase
+{
+    public override void Render(Graphics g)
+        => g.DrawLine(CurrentPen, Location, Location + Size);
+}
